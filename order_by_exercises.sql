93b8cd8bff1f7586c35b66fc4fb157518a7bcb7e
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-20 12:01:21 -0500

    finished order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+SELECT * 
+FROM employees 
+WHERE (first_name = 'Irena' 
+OR first_name = 'Vidya' 
+OR first_name = 'Maya')
+AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT * 
+FROM employees 
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT * 
+FROM employees 
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
